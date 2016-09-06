0f1112dfbf46be264588a35ef47ad9bf33e99d80
m.rajaonarison <m.rajaonarison@etechconsulting-mg.com>
2016-09-06 21:38:10 +0300

    Commit

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 define("CLIENT_ID", "test-vz8bxd");
 define("CLIENT_SECRET","anwEF1w3nnXLkb7BGcIQXf60NJeTo4HJr9A9aivFxHJQuqbOpkv0CoiNqD7tNL8J");
 
-define("APP_ID",16608075);
-define("APP_TOKEN","f633992f16c74c9787405b4afc1f793b");
+define("APP_ID",13781550);
+define("APP_TOKEN","5959667d7fa944cbbd4351c6cdb66158");
 
 define("APP_ID_MILES",13781571);
 define("APP_TOKEN_MILES","5aac7675bd814c799accd4ddddb2170f");
